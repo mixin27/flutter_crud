eb496637082b78abf4325f3aab00163f6313f675
Kyaw Zayar Tun <kyawzayartun.sbs@gmail.com>
2022-09-14 14:02:05 +0630

    config theme and theme mode providers

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,16 +1,31 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/core/feat_core.dart';
 import 'package:flutter_crud/routes/app_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>(
+  (ref) async {
+    // initialization stuffs
+    return unit;
+  },
+);
+
+class AppWidget extends ConsumerWidget {
   AppWidget({super.key});
 
   final _appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (previous, next) {});
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'CRUD Demo',
+      theme: ref.watch(lightThemeProvider),
+      darkTheme: ref.watch(darkThemeProvider),
+      themeMode: ref.watch(themeModeProvider),
       routeInformationParser: _appRouter.defaultRouteParser(),
       routerDelegate: _appRouter.delegate(),
     );
